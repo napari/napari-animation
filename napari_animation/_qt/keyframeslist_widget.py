61a09bbf4849f1bc8639ef6f6f9e822f2a9f0e65
Alister Burt <alisterburt@gmail.com>
2021-01-14 09:48:35 +0000

    add mechanism for updating KeyFramesListWidget from the Animation object.

--- napari_animation/_qt/keyframeslist_widget.py
+++ napari_animation/_qt/keyframeslist_widget.py
@@ -1,6 +1,6 @@
 from qtpy.QtWidgets import QWidget, QHBoxLayout
 
-from qtpy.QtWidgets import QListWidget, QFormLayout, QPushButton
+from qtpy.QtWidgets import QListWidget, QFormLayout, QAbstractItemView, QPushButton
 
 # NOT IMPLEMENTED YET
 class KeyFramesListWidget(QListWidget):
@@ -9,16 +9,8 @@ class KeyFramesListWidget(QListWidget):
     def __init__(self, animation, parent=None):
         super().__init__(parent=parent)
 
-        self._layout = QFormLayout(parent=self)
-
         self.animation = animation
-
-        self.keyframeslist = QListWidget()
-        self.keyframeslist.itemClicked.connect(self._click_item_callback)
-        self.keyframeslist.itemDoubleClicked.connect(self._doubleclick_item_callback)
-
-        self._layout.addRow(self.keyframeslist)
-
+        self.setDragDropMode(QAbstractItemView.InternalMove)
         self._id_to_label = {}
 
     def add_current_keyframe(self):
@@ -26,48 +18,48 @@ class KeyFramesListWidget(QListWidget):
         """
         key_frame_id = id(self.animation.key_frames[self.animation.frame])
         label = f'key frame {self.animation.frame}'
-        self.keyframeslist.addItem(label)
+        self.addItem(label)
         self._id_to_label[key_frame_id] = label
 
     def _update_from_animation(self):
-        """update keyframelist with labels from animation
+        """update GUI state from self.animation state
         """
-        labels = [self._id_to_label[id(obj)] for obj in self.animation.key_frames]
         self.keyframeslist.clear()
-        self.keyframeslist.addItems(labels)
+        self.keyframeslist.addItems(self.key_frame_labels)
+
+    def dropEvent(self, event):
+        """update animation state on 'drop' of frame in key frames list
+        """
+        super().dropEvent(event)
+        self._update_animation()
 
     def _update_animation(self):
-        """push state of keyframeslist to self.animation
+        """push current GUI state to self.animation
         """
-        pass
-
-    def _click_item_callback(self, item):
-        pass
-
-    def _doubleclick_item_callback(self, item):
-        """on doubleclick, activate changing parameters for the current key frame
-        """
-        pass
-
-    def _move_item_callback(self):
-        """on move item, update state of animation.key_frames
-        """
-
-    def _delete_item_callback(self):
-        """on delete item, update state of animation.key_frames
-        """
+        new_key_frames = [self.animation_state_map[label] for label in self.gui_labels]
+        self.animation.key_frames = new_key_frames
 
     @property
-    def _keyframeslist_items(self):
+    def items(self):
         """iterate over items in the keyframes list
         """
-        for i in range(self.keyframeslist.count()):
-            yield self.keyframeslist.item(i)
+        for i in range(self.count()):
+            yield self.item(i)
 
     @property
-    def _keyframeslist_text(self):
-        """text representation items in keyframes list
+    def gui_labels(self):
+        """text representation of items in keyframes list
         """
-        for item in self._keyframeslist_items:
+        for item in self.items:
             yield item.text()
 
+    @property
+    def key_frame_labels(self):
+        """current gui_labels according to key frames of animation
+        """
+        return [self._id_to_label[id(obj)] for obj in self.animation.key_frames]
+
+    @property
+    def animation_state_map(self):
+        return {label : state for label, state in zip(self.key_frame_labels,
+                                                      self.animation.key_frames)}
