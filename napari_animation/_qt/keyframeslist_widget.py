7d2655816c915a921f7ce996c2d620109516df93
Alister Burt <alisterburt@gmail.com>
2021-01-17 18:08:47 +0000

    labelling and insertion at selected position working

--- napari_animation/_qt/keyframeslist_widget.py
+++ napari_animation/_qt/keyframeslist_widget.py
@@ -6,13 +6,24 @@ from napari.utils.events import EventedList
 
 # NOT IMPLEMENTED YET
 class KeyFramesListWidget(QListWidget):
-    """List of Key Frames.
+    """List of key frames for an animation
+
+    Parameters
+    ----------
+    animation : napari_animation.Animation
+        napari-animation animation to be synced with the GUI.
+
+    Attributes
+    ----------
+    animation : napari_animation.Animation
+        napari-animation animation in sync with the GUI.
     """
     def __init__(self, animation, parent=None):
         super().__init__(parent=parent)
 
         self.animation = animation
-        self._id_to_label = {}
+        self._frame_counter = 0
+        self._key_frame_id_to_label = {}
 
         self._connect_key_frame_events()
         self.setDragDropMode(super().InternalMove)
@@ -33,18 +44,16 @@ class KeyFramesListWidget(QListWidget):
         super().dropEvent(event)
         self._update_backend()
 
+    def _on_click_callback(self, item):
+        self._update_animation_frame()
+
     def add_key_frame(self, *args):
         """Generate list item for current keyframe and store its unique id
         """
         key_frame_idx = self.animation.frame
         self.insertItem(key_frame_idx, self._generate_list_item(key_frame_idx))
         self._store_key_frame_id(key_frame_idx)
-
-    def _on_click_callback(self, item):
-        """
-        """
-        selected_index = self.indexFromItem(item).row()
-        self.animation.frame = selected_index
+        self._frame_counter += 1
 
     def _update_backend(self):
         """push current GUI state to self.animation
@@ -56,6 +65,7 @@ class KeyFramesListWidget(QListWidget):
         # which fire when we modify the list in place
         self.animation.key_frames = EventedList(new_key_frames)
         self._connect_key_frame_events()
+        self._update_animation_frame()
 
     def _update_frontend(self, *args):
         """update GUI state from self.animation state
@@ -65,6 +75,13 @@ class KeyFramesListWidget(QListWidget):
         self.clear()
         for idx, _ in enumerate(self.frontend_items):
             self.addItem(self._generate_list_item(idx))
+
+    def _update_animation_frame(self):
+        """update the frame number of self.animation based on selected item in frontend
+        """
+        [item] = self.selectedItems()
+        selected_index = self.indexFromItem(item).row()
+        self.animation.frame = selected_index
 
     def _generate_list_item(self, key_frame_idx):
         """Generate a QListWidgetItem from a key frame at key_frame_idx
@@ -78,7 +95,7 @@ class KeyFramesListWidget(QListWidget):
         """Store the unique id of the key frame at key_frame_idx in a dict of {id: key_frame_label}
         """
         key_frame_id = id(self._get_key_frame(key_frame_idx))
-        self._id_to_label[key_frame_id] = self._generate_label(key_frame_idx)
+        self._key_frame_id_to_label[key_frame_id] = self._generate_label(key_frame_idx)
 
     def _icon_from_key_frame(self, key_frame):
         """Generate QIcon from a key frame
@@ -99,9 +116,15 @@ class KeyFramesListWidget(QListWidget):
         return self.animation.key_frames[key_frame_idx]
 
     def _generate_label(self, key_frame_idx):
-        """Generate a label for a given list index
+        """Generate a label for a given key frame list index
         """
-        return f'key frame {key_frame_idx}'
+        key_frame_id = id(self._get_key_frame(key_frame_idx))
+
+        if key_frame_id in self._key_frame_id_to_label.keys():
+            label = self._key_frame_id_to_label[key_frame_id]
+        else:
+            label = f'key frame {self._frame_counter}'
+        return label
 
     def _label_to_key_frame(self, label):
         """gets the key frame associated with a given label in the frontend
@@ -126,7 +149,7 @@ class KeyFramesListWidget(QListWidget):
     def backend_key_frame_labels(self):
         """labels for key frames in the order present in the backend
         """
-        return [self._id_to_label[id(key_frame)] for key_frame in self.animation.key_frames]
+        return [self._key_frame_id_to_label[id(key_frame)] for key_frame in self.animation.key_frames]
 
     @property
     def labels_to_key_frames(self):
