ee35857be8685ff7f7791893769066e1c5eb20bf
Nicholas Sofroniew <sofroniewn@gmail.com>
2021-01-31 15:56:44 -0800

    add hookimpls

---
+++ napari_animation/_hookimpls.py
@@ -0,0 +1,7 @@
+from napari_plugin_engine import napari_hook_implementation
+from ._qt import AnimationWidget
+
+
+@napari_hook_implementation
+def napari_experimental_provide_dock_widget():
+    return AnimationWidget
