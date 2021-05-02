8f42a78231d83f1a9f0c98e1dc614a1b1b2ffe3f
Fifourche <faris.abouakil@gmail.com>
2021-05-02 20:28:55 +0200

    Modified the utils tests (to test nested dictionaries), and animation tests (removed the zoom specific test).

--- napari_animation/_tests/test_utils.py
+++ napari_animation/_tests/test_utils.py
@@ -1,8 +1,40 @@
+import numbers
+
+import numpy as np
 import pytest
 
 from ..utils import interpolate_state, keys_to_list, nested_get
 
 
+# Define some functions used for testing
+def nested_assert_close(a, b):
+    """ Assert close on nested dicts."""
+    a_keys = [key for key in keys_to_list(a)]
+    b_keys = [key for key in keys_to_list(b)]
+
+    assert a_keys == b_keys
+
+    for key in a_keys:
+        a_1 = nested_get(a, key)
+        b_1 = nested_get(b, key)
+
+        nested_seq_assert_close(a_1, b_1)
+
+
+def nested_seq_assert_close(a, b):
+    """ Assert close to scalar or potentially nested qequences of numeric types and others."""
+    if isinstance(a, (list, tuple)) and isinstance(b, (list, tuple)):
+        assert type(a) == type(b)
+        for a_v, b_v in zip(a, b):
+            nested_seq_assert_close(a_v, b_v)
+    else:
+        if isinstance(a, numbers.Number):
+            np.testing.assert_allclose(a, b)
+        else:
+            assert a == b
+
+
+# Actual tests
 @pytest.mark.parametrize("fraction", [0, 0.2, 0.4, 0.6, 0.8, 1])
 def test_interpolate_state(key_frames, fraction):
     """Check that state interpolation works"""
@@ -11,9 +43,12 @@ def test_interpolate_state(key_frames, fraction):
     result = interpolate_state(initial_state, final_state, fraction)
     assert len(result) == len(initial_state)
     if fraction == 0:
-        assert result == initial_state
+        # assert result == initial_state
+        nested_assert_close(result, initial_state)
     elif fraction == 1:
-        assert result == final_state
+        # assert result == final_state
+        nested_assert_close(result, final_state)
+
     # else:
     # should find something else to test
 
