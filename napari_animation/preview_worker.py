69208b1caec450128a0a497ec4ceda2bfabd9863
Fifourche <faris.abouakil@gmail.com>
2021-07-20 11:05:11 +0200

    Finishing merge with main.

--- napari_animation/preview_worker.py
+++ napari_animation/preview_worker.py
@@ -24,10 +24,12 @@ class PreviewWorker(QObject):
         self.animation = animation
         self.interval = 1000 / abs(fps)
         self.frame_range = frame_range
-        self.current_frame = frame_range[0]
+        self.current_index = frame_range[0]
 
         # ÷ so that current_frame follows the last set frame index
-        self.animation.events._set_frame_index.connect(self._on_frame_changed)
+        self.animation._frames.events._current_index.connect(
+            self._on_frame_changed
+        )
 
         self.timer = QTimer()
 
@@ -35,7 +37,7 @@ class PreviewWorker(QObject):
     def work(self):
         """Play the animation."""
 
-        self.frame_requested.emit(self.current_frame)
+        self.frame_requested.emit(self.current_index)
         self.timer.singleShot(int(self.interval), self.advance)
         self.advance()
         self.started.emit()
@@ -45,19 +47,19 @@ class PreviewWorker(QObject):
         Restricts the animation to the requested frame_range,
         if entered.
         """
-        self.current_frame += 1
+        self.current_index += 1
 
-        if self.current_frame > self.frame_range[1]:
+        if self.current_index > self.frame_range[1]:
             return self.finish()
 
-        self.frame_requested.emit(self.current_frame)
+        self.frame_requested.emit(self.current_index)
         self.timer.singleShot(int(self.interval), self.advance)
 
     @Slot(Event)
     def _on_frame_changed(self, event):
         """Update the current frame if the set frame has changed (from slider or programmatically)."""
         # slot for external events to update the current frame
-        self.current_frame = event.value
+        self.current_index = event.value
 
     def finish(self):
         """Emit the finished event signal."""
