f2c68fe6c6ffc876dfe28e87d3eacffcc3d16a7e
alisterburt <alisterburt@gmail.com>
2022-11-12 16:46:50 +0000

    fix for tracks color-by bug, don't track properties

--- napari_animation/viewer_state.py
+++ napari_animation/viewer_state.py
@@ -32,6 +32,7 @@ class ViewerState:
         }
         for layer_attributes in layers.values():
             layer_attributes.pop("metadata")
+            layer_attributes.pop("properties", None)
         return cls(
             camera=viewer.camera.dict(), dims=viewer.dims.dict(), layers=layers
         )
