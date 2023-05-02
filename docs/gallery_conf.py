f235b6bab9c10c45c73e0b14491f81ee7a01d52c
melissawm <melissawm@gmail.com>
2023-05-02 16:16:03 -0300

    Fix pre-commit warnings

--- docs/gallery_conf.py
+++ docs/gallery_conf.py
@@ -1,11 +1,19 @@
-from mkdocs_gallery.gen_gallery import DefaultResetArgv
-import qtgallery
 from pathlib import Path
 
+import qtgallery
+from mkdocs_gallery.gen_gallery import DefaultResetArgv
+
 conf = {
-    'reset_argv': DefaultResetArgv(),
-    'default_thumb_file': Path(__file__).parent / 'assets' / 'images' / 'logo.png',
-    #'image_scrapers': ("matplotlib", qtgallery.qtscraper,),
-    'remove_config_comments': True,
-    #'run_stale_examples': True,
+    "reset_argv": DefaultResetArgv(),
+    "default_thumb_file": Path(__file__).parent
+    / "assets"
+    / "images"
+    / "logo.png",
+    "image_scrapers": (
+        "matplotlib",
+        qtgallery.qtscraper,
+    ),
+    "reset_modules": (qtgallery.reset_qapp,),
+    "remove_config_comments": True,
+    "run_stale_examples": True,
 }
