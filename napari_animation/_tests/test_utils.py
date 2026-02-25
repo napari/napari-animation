21f7f5304e184f546c1ec5eeeaa822182a648818
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2026-02-25 09:45:12 -0500

    Add regression test

--- napari_animation/_tests/test_utils.py
+++ napari_animation/_tests/test_utils.py
@@ -1,6 +1,8 @@
 import pytest
+from napari.utils.colormaps import DirectLabelColormap
 
 from ..interpolation.utils import keys_to_list, nested_get
+from ..utils import layer_attribute_changed
 
 input_dict = [{"a": 1, "b": {"c": "d"}}]
 keys = [["b", "c"]]
@@ -25,3 +27,25 @@ def test_keys_to_list(input_dict, expected):
     for keys in result:
         assert isinstance(keys, list)
     assert result == expected
+
+
+def test_layer_attribute_changed_direct_label_colormap_equal():
+    cmap1 = DirectLabelColormap(
+        color_dict={None: 'black', 1: 'red', 2: 'blue'}
+    )
+    cmap2 = DirectLabelColormap(
+        color_dict={None: 'black', 1: 'red', 2: 'blue'}
+    )
+
+    assert not layer_attribute_changed(cmap1, cmap2)
+
+
+def test_layer_attribute_changed_direct_label_colormap_changed():
+    cmap1 = DirectLabelColormap(
+        color_dict={None: 'black', 1: 'red', 2: 'blue'}
+    )
+    cmap2 = DirectLabelColormap(
+        color_dict={None: 'black', 1: 'red', 2: 'green'}
+    )
+
+    assert layer_attribute_changed(cmap1, cmap2)
