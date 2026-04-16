495eea0b5e1d7ec0ebb9d440ca5d80b89da56566
Lorenzo Gaifas <brisvag@gmail.com>
2026-04-16 17:17:44 +0200

    add tests

--- src/napari_animation/_qt/_tests/test_animation_widget.py
+++ src/napari_animation/_qt/_tests/test_animation_widget.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from napari_animation._qt import AnimationWidget
+from napari_animation._qt import AnimationTimelineWidget, AnimationWidget
 
 
 def test_animation_widget(make_napari_viewer, qtbot):
@@ -17,3 +17,19 @@ def test_animation_widget(make_napari_viewer, qtbot):
     assert len(aw.animation.key_frames) == 2
     aw._delete_keyframe_callback()
     assert len(aw.animation.key_frames) == 1
+
+
+def test_animation_timeline_widget(make_napari_viewer, qtbot):
+    viewer = make_napari_viewer()
+    viewer.add_image(np.random.random((28, 28)))
+    aw = AnimationTimelineWidget(viewer)
+    qtbot.addWidget(aw)
+
+    animation = aw.timeline.animation
+    track = animation.add_track('viewer')
+    animation.add_keyframe_from_state('viewer', 0)
+    assert len(track.keyframes) == 1
+    animation.add_keyframe_from_state('viewer', 10)
+    assert len(track.keyframes) == 2
+    animation.add_keyframe_from_state('viewer', 20)
+    assert len(track.keyframes) == 3
