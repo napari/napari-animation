61dc185c5544df07d071426e5a91d365292784ce
Lorenzo Gaifas <brisvag@gmail.com>
2026-04-17 15:11:25 +0200

    add test

--- src/napari_animation/_qt/_tests/test_animation_widget.py
+++ src/napari_animation/_qt/_tests/test_animation_widget.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import numpy as np
 
 from napari_animation._qt import AnimationTimelineWidget, AnimationWidget
@@ -27,9 +29,39 @@ def test_animation_timeline_widget(make_napari_viewer, qtbot):
 
     animation = aw.timeline.animation
     track = animation.add_track('viewer')
+    viewer.camera.zoom = 10.0
     animation.add_keyframe_from_state('viewer', 0)
     assert len(track.keyframes) == 1
+    viewer.camera.zoom = 20.0
     animation.add_keyframe_from_state('viewer', 10)
     assert len(track.keyframes) == 2
     animation.add_keyframe_from_state('viewer', 20)
     assert len(track.keyframes) == 3
+
+    animation.current_frame = 5
+    assert viewer.camera.zoom == 15.0
+
+
+def test_timeline_custom_track(make_napari_viewer, qtbot):
+    viewer = make_napari_viewer()
+    viewer.add_image(np.random.random((28, 28)))
+    aw = AnimationTimelineWidget(viewer)
+    qtbot.addWidget(aw)
+
+    @dataclass
+    class Thing:
+        x: int = 1
+
+    thing = Thing()
+
+    aw.add_custom_track('x', thing, 'x')
+    animation = aw.timeline.animation
+
+    track = animation.add_track('x')
+    animation.add_keyframe_from_state('x', 0)
+    assert track.keyframes[0].t == 0
+    assert track.keyframes[0].value == 1
+    thing.x = 2
+    animation.add_keyframe_from_state('x', 10)
+    assert track.keyframes[1].t == 10
+    assert track.keyframes[1].value == 2
